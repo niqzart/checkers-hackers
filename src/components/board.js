32ef5738d9f5058842f39ecb1e964a5dcd8ba58c
qzart45hi <qwert45hi@yandex.ru>
2021-09-04 01:33:40 +0300

    moving square out of board.js

--- src/components/board.js
+++ src/components/board.js
@@ -1,23 +1,7 @@
 import React from "react"
 
 import "./board.css"
-import Checker from "./pieces"
-
-function Square({ id, even, over, draged, game, checker }) {
-  return <td
-    key={id}
-    className={"square"}
-    onDragStart={() => game.handleStartDrag(id)}
-    onDragEnter={() => game.handleOver(id)}
-    onDragEnd={() => game.handleEndDrag(id)}
-    onDoubleClick={() => game.handleDoubleClick(id)}
-    // style={{ backgroundColor: over ? (game.canMovePieceTo(id) ? "blue" : "red") : (even ? "#FFCE9E" : "#D18B47") }}>
-    style={{ backgroundColor: over && game.canMovePieceTo(id) ? "blue" : (even ? "#FFCE9E" : "#D18B47") }}>
-    <div style={{ opacity: draged ? 0 : 1 }}>
-      {checker === null ? null : <Checker white={checker.white} king={checker.king} />}
-    </div>
-  </td>
-}
+import Square from "./square"
 
 function Row({ number, game, width, flip, height, positions }) {
   const y = number
---
+++ src/components/square.js
@@ -0,0 +1,17 @@
+import Checker from "./pieces"
+
+export default function Square({ id, even, over, draged, game, checker }) {
+  return <td
+    key={id}
+    className={"square"}
+    onDragStart={() => game.handleStartDrag(id)}
+    onDragEnter={() => game.handleOver(id)}
+    onDragEnd={() => game.handleEndDrag(id)}
+    onDoubleClick={() => game.handleDoubleClick(id)}
+    // style={{ backgroundColor: over ? (game.canMovePieceTo(id) ? "blue" : "red") : (even ? "#FFCE9E" : "#D18B47") }}>
+    style={{ backgroundColor: over && game.canMovePieceTo(id) ? "blue" : (even ? "#FFCE9E" : "#D18B47") }}>
+    <div style={{ opacity: draged ? 0 : 1 }}>
+      {checker === null ? null : <Checker white={checker.white} king={checker.king} />}
+    </div>
+  </td>
+}
