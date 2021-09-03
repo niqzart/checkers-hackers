49b28c88570ab61000f8211935b212a61cc3d888
qzart45hi <qwert45hi@yandex.ru>
2021-09-03 19:13:23 +0300

    lobby page

---
+++ src/pages/lobby.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { CopyToClipboard } from "react-copy-to-clipboard"
+
+import { Grid, CircularProgress, IconButton } from "@material-ui/core"
+import VisibilityIcon from '@material-ui/icons/Visibility'
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff'
+import { Redirect } from "react-router"
+
+
+export default class LobbyPage extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      codeShown: false,
+      gameStarted: false,
+    }
+  }
+
+  render() {
+    const lobbySettings = this.props.location.state
+    const hasCode = lobbySettings.code !== null
+
+    if (this.state.gameStarted) return <Redirect to={{ pathname: "/games/", state: lobbySettings }} push />
+    return <Grid container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Grid item xs={12} style={{ textAlign: "center" }}>
+        <h1>Waiting for your opponent...</h1>
+        <h2>To invite someone, send them lobby id: {this.props.location.state.gameID}</h2>
+        {hasCode ? <h2>Lobby code: {this.state.codeShown ? lobbySettings.code : "•••••"}
+          <IconButton size="small" onClick={() => this.setState({ codeShown: !this.state.codeShown })}>
+            {this.state.codeShown ? <VisibilityIcon /> : <VisibilityOffIcon />}
+          </IconButton>
+        </h2> : null}
+      </Grid>
+      <Grid item xs={12}>
+        <CircularProgress thickness={3} size={70} />
+      </Grid>
+    </Grid>
+  }
+}
