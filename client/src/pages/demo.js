cb033fb9c67ffbda0a3c8a6951e1235dd7247575
qzart45hi <qwert45hi@yandex.ru>
2021-09-19 00:29:41 +0300

    making demo page better

--- client/src/pages/demo.js
+++ client/src/pages/demo.js
@@ -3,20 +3,18 @@ import { useLocation } from "react-router";
 import Game from "./game";
 
 export default function DemoPage() {
-  const host = "s://checkers-hackers-server.herokuapp.com"
-  // const host = "://localhost:4000"
-
   const query = new URLSearchParams(useLocation().search)
   const flip = query.get("flip")
   const gametype = query.get("gametype")
 
   const ws = {
-    send: (message) => console.log("Trying to send:\n" + message)
+    send: (message) => console.log("Trying to send:\n" + message),
+    close: () => console.log("Trying to send:\nCLOSED"),
   }
 
   return <Game
     ws={ws}
-    flip={flip === null ? false : flip}  // use query string instead
-    gametype={gametype === null ? 1 : gametype}  // use query string instead
+    flip={flip === "true" ? true : false}  // use query string instead
+    gametype={gametype === null ? 1 : parseInt(gametype)}  // use query string instead
   />
 }
